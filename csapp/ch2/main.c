e3a0e8f0a7fc0e44bb5865436ecab119e76faf9d
tan <tjiajing@gmail.com>
2014-03-23 22:48:34 +0800

    bit_negative exercises

--- csapp/ch2/main.c
+++ csapp/ch2/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+#define float_bits unsigned int
+
+float_bits float_negate(float_bits f){
+    unsigned sign = !(f >> 31);
+    unsigned exp = (f >> 22) & 0xFF;
+    unsigned frac = f & 0x7FFFFF;
+    // if f is Nan
+    if (exp == 0xFF && frac != 0){
+        sign = f >> 31;
+        }
+    return (sign << 31) | (exp << 23) | frac;
+
+}
+
 int main(void)
 {
-    printf("Hello World!\n");
-    return 0;
+    unsigned int negate = float_negate(0xFFFFFFFF);
+    printf("The negative of the float number is %.x \n",negate);
 }
+
 
