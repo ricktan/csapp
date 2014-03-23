ef19948ea8f5685b1a24e087ec963fbacedac8c7
tan <tjiajing@gmail.com>
2014-03-23 16:38:06 +0800

    creat an empty prject for the ch2

---
+++ csapp/ch2-build/CMakeFiles/ch2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ch2.dir/main.c.o"
+  "ch2.pdb"
+  "ch2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/ch2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
